16d67b83f9104fea157d2d446e811997c5f42a2d
fensken <shardul.gautam123@gmail.com>
2024-04-10 20:31:34 +0545

    fix: navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
         </p>
       </Link>
 
-      <div className="flex-between gap-5">
+      <div className="flex-between gap-5 sm:gap-0">
         <SignedIn>
           <UserButton />
         </SignedIn>
